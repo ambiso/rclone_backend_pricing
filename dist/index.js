ac656a05c342e0cc1d7971b331674aca8c84ee79
ambiso <ambiso@invalid>
2020-11-14 11:05:20 +0100

    Implement Backblaze B2 backend
    This deviates from the backblaze b2 calculator https://www.backblaze.com/b2/cloud-storage-pricing.html
    in at least the following way:
    We currently handle data deleted/uploaded
    per month differently.
    When you delete more than you upload,
    you will always have at least as much as you uploaded
    in your account.

--- dist/index.js
+++ dist/index.js
@@ -32,13 +32,17 @@ class TieredPlan {
         this.currency = currency;
     }
 }
-function fichier_extra_cost(plan, storage) {
+function fichier_extra_cost(constraints, plan, storage) {
     if (storage < 100 * 1000) { // Less than 100 TB
-        return Math.max(0, Math.floor((storage - 2000) / 1000) * 100);
+        return Math.max(0, Math.ceil((storage - 2000) / 1000));
     }
     else {
         return Infinity;
     }
+}
+function backblaze_b2_extra_cost(constraints, plan, storage) {
+    console.log(storage);
+    return storage * 0.005 + 0.01 * constraints.download_mo;
 }
 function sample(array) {
     return array[Math.floor(Math.random() * array.length)];
@@ -47,24 +51,28 @@ var providers = [
     new Provider("1fichier", "https://1fichier.com/", [
         "https://1fichier.com/tarifs.html?af=3676346"
     ], [
-        new TieredPlan("Premium 1 month", 1, 300, 2000, fichier_extra_cost, Currency.EUR),
-        new TieredPlan("Premium 1 year", 12, 2200, 2000, fichier_extra_cost, Currency.EUR),
-        new TieredPlan("Premium 5 years", 12 * 5, 9900, 2000, fichier_extra_cost, Currency.EUR),
-        new TieredPlan("Premium 10 years", 12 * 10, 19500, 2000, fichier_extra_cost, Currency.EUR),
+        new TieredPlan("Premium 1 month", 1, 3.00, 2000, fichier_extra_cost, Currency.EUR),
+        new TieredPlan("Premium 1 year", 12, 22.00, 2000, fichier_extra_cost, Currency.EUR),
+        new TieredPlan("Premium 5 years", 12 * 5, 99.00, 2000, fichier_extra_cost, Currency.EUR),
+        new TieredPlan("Premium 10 years", 12 * 10, 195.00, 2000, fichier_extra_cost, Currency.EUR),
+    ]),
+    new Provider("Backblaze", "https://www.backblaze.com/b2/cloud-storage-pricing.html", [], [
+        new TieredPlan("B2", 1, 0, 0, backblaze_b2_extra_cost, Currency.USD),
     ])
 ];
 function compute_storage_requirements(constraints) {
-    let current = 0;
+    let current = constraints.initial_upload;
     let storage_for_months = [];
     for (let i = 0; i < constraints.months; ++i) {
-        current += constraints.upload_mo - constraints.download_mo;
+        current -= constraints.delete_mo;
         current = Math.max(current, 0);
+        current += constraints.upload_mo;
         storage_for_months.push(current);
     }
     return storage_for_months;
 }
 /// Returns a list of tiered plans to subscribe to in succession
-function choose_tiered_plan(provider, storage_for_months, constraints) {
+function choose_tiered_plan(constraints, provider, storage_for_months) {
     let cost_so_far = new Array(constraints.months + 1).fill(Infinity);
     let backlinks = new Array(constraints.months + 1); // Which plan we took
     cost_so_far[0] = 0;
@@ -75,7 +83,7 @@ function choose_tiered_plan(provider, storage_for_months, constraints) {
             }
             let extra_cost = 0;
             for (let m = i - plan.months; m < i; ++m) {
-                extra_cost += plan.extra_cost(plan, storage_for_months[m]);
+                extra_cost += plan.extra_cost(constraints, plan, storage_for_months[m]);
             }
             let cost_this_plan = cost_so_far[i - plan.months] + plan.cost + extra_cost;
             if (cost_this_plan < cost_so_far[i]) {
@@ -103,9 +111,10 @@ function recompute() {
     let enterprise = document.getElementById("enterprise").value === "on";
     let constraints = new UserInput(months, currency, initial_upload, upload_mo, delete_mo, download_mo, enterprise);
     let storage_for_months = compute_storage_requirements(constraints);
+    console.log(storage_for_months);
     let provider_plans = [];
     for (let provider of providers) {
-        provider_plans.push(choose_tiered_plan(provider, storage_for_months, constraints));
+        provider_plans.push(choose_tiered_plan(constraints, provider, storage_for_months));
     }
     provider_plans.sort((a, b) => a[1] - b[1]); // sort by price, decending
     let results_table = document.getElementById("results");
--- src/index.ts
+++ src/index.ts
@@ -30,17 +30,22 @@ class TieredPlan {
       readonly months: number,
       readonly cost: number,
       readonly storage_cap: number,
-      readonly extra_cost: (plan: TieredPlan, storage: number) => number,
+      readonly extra_cost: (constraints: UserInput, plan: TieredPlan, storage: number) => number,
       readonly currency: Currency
     ) {}
 }
 
-function fichier_extra_cost(plan: TieredPlan, storage: number) {
+function fichier_extra_cost(constraints: UserInput, plan: TieredPlan, storage: number) {
   if (storage < 100*1000) { // Less than 100 TB
-    return Math.max(0, Math.floor((storage-2000)/1000)*100);
+    return Math.max(0, Math.ceil((storage-2000)/1000));
   } else {
     return Infinity;
   }
+}
+
+function backblaze_b2_extra_cost(constraints: UserInput, plan: TieredPlan, storage: number) {
+  console.log(storage);
+  return storage * 0.005 + 0.01 * constraints.download_mo;
 }
 
 function sample<T>(array: Array<T>): T {
@@ -55,27 +60,36 @@ var providers = [
       "https://1fichier.com/tarifs.html?af=3676346"
     ],
     [
-      new TieredPlan("Premium 1 month", 1, 300, 2000, fichier_extra_cost, Currency.EUR),
-      new TieredPlan("Premium 1 year", 12, 2200, 2000, fichier_extra_cost, Currency.EUR),
-      new TieredPlan("Premium 5 years", 12*5, 9900, 2000, fichier_extra_cost, Currency.EUR),
-      new TieredPlan("Premium 10 years", 12*10, 19500, 2000, fichier_extra_cost, Currency.EUR),
+      new TieredPlan("Premium 1 month", 1, 3.00, 2000, fichier_extra_cost, Currency.EUR),
+      new TieredPlan("Premium 1 year", 12, 22.00, 2000, fichier_extra_cost, Currency.EUR),
+      new TieredPlan("Premium 5 years", 12*5, 99.00, 2000, fichier_extra_cost, Currency.EUR),
+      new TieredPlan("Premium 10 years", 12*10, 195.00, 2000, fichier_extra_cost, Currency.EUR),
+    ]
+  ),
+  new Provider(
+    "Backblaze",
+    "https://www.backblaze.com/b2/cloud-storage-pricing.html",
+    [],
+    [
+      new TieredPlan("B2", 1, 0, 0, backblaze_b2_extra_cost, Currency.USD),
     ]
   )
 ];
 
 function compute_storage_requirements(constraints: UserInput): number[] {
-  let current = 0;
+  let current = constraints.initial_upload;
   let storage_for_months = [];
   for (let i = 0; i < constraints.months; ++i) {
-    current += constraints.upload_mo - constraints.download_mo;
+    current -= constraints.delete_mo;
     current = Math.max(current, 0);
+    current += constraints.upload_mo;
     storage_for_months.push(current);
   }
   return storage_for_months;
 }
 
 /// Returns a list of tiered plans to subscribe to in succession
-function choose_tiered_plan(provider: Provider, storage_for_months: number[], constraints: UserInput): [Provider, TieredPlan[], number] {
+function choose_tiered_plan(constraints: UserInput, provider: Provider, storage_for_months: number[]): [Provider, TieredPlan[], number] {
   let cost_so_far = new Array(constraints.months+1).fill(Infinity);
   let backlinks: TieredPlan[] = new Array(constraints.months+1); // Which plan we took
 
@@ -89,7 +103,7 @@ function choose_tiered_plan(provider: Provider, storage_for_months: number[], co
 
       let extra_cost = 0;
       for (let m = i-plan.months; m < i; ++m) {
-        extra_cost += plan.extra_cost(plan, storage_for_months[m]);
+        extra_cost += plan.extra_cost(constraints, plan, storage_for_months[m]);
       }
 
       let cost_this_plan = cost_so_far[i-plan.months] + plan.cost + extra_cost;
@@ -120,10 +134,11 @@ function recompute() {
 
   let constraints = new UserInput(months, currency, initial_upload, upload_mo, delete_mo, download_mo, enterprise);
   let storage_for_months = compute_storage_requirements(constraints);
+  console.log(storage_for_months);
 
   let provider_plans = [];
   for (let provider of providers) {
-    provider_plans.push(choose_tiered_plan(provider, storage_for_months, constraints));
+    provider_plans.push(choose_tiered_plan(constraints, provider, storage_for_months));
   }
   provider_plans.sort((a, b) => a[1] - b[1]); // sort by price, decending
 
